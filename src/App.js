84838326c31381903d445386933a3fb23663425f
itgirlsufpb <itgirls@dcx.ufpb.br>
2020-06-30 18:02:55 -0300

    Commit Geral

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import MenuITGirls from './components/menu';
+
+ 
+function App() {
+  return (
+    <div className="App"> 
+      <MenuITGirls/>
+    </div>
+  );
+}
+
+export default App;
